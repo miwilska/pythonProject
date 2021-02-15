56cf3fdfe6a700d3bda1713605fa8727d68e85a0
miwilska <22717834+miwilska@users.noreply.github.com>
2021-02-15 18:40:39 +0100

    files and copy  time and songs

--- kopiowanie-cwiczenie.py
+++ kopiowanie-cwiczenie.py
@@ -1,46 +1,56 @@
 import random
 import copy
+# https://analityk.edu.pl/kopiowanie-list-i-slownikow/
 
 class Play:
-    def __init__(self,path:str):
-        if not isinstance(path,str):
+    def __init__(self, path: str):
+        if not isinstance(path, str):
+            print("Coś tu nie gra")
             raise TypeError
         else:
             print("Gra muzyka")
 
-#baza_slownik {id: [id,ścieżka,tytuł, wykonawca, liczba odsłuchań]}
-#zmieniamy liczbę odsłuchań oraz dodajemy piosenkę, jeśli nie ma jej w bazie
+
+# baza_słownik {id: [id,ścieżka,tytuł, wykonawca, liczba odsłuchań]}
+# zmieniamy liczbę odsłuchań oraz dodajemy piosenkę, jeśli nie ma jej w bazie
 
 with open("Dane/baza_piosenek.txt", "r", -1, "utf-8") as file:
-    base_dict={line.strip().split(";")[0]:line.strip().split(";") for line in file.readlines()}
+    base_dict = {line.strip().split(";")[0]: line.strip().split(";") for line in file.readlines()}
 
-playlist_base={
-            "Różne":
-                [base_dict['path2.mp3'],base_dict['path7.mp3'],base_dict['path9.mp3'],
-                base_dict['path10.mp3'],base_dict['path18.mp3'],base_dict['path19.mp3']],
-            "Instrumental":
-                [base_dict['path3.mp3'],base_dict['path4.mp3'],base_dict['path8.mp3'],
-                base_dict['path16.mp3'],base_dict['path20.mp3'],base_dict['path21.mp3']]}
+playlist_base = {
+    "Różne":
+        [base_dict['path2.mp3'], base_dict['path7.mp3'], base_dict['path9.mp3'],
+         base_dict['path10.mp3'], base_dict['path18.mp3'], base_dict['path19.mp3']],
+    "Instrumental":
+        [base_dict['path3.mp3'], base_dict['path4.mp3'], base_dict['path8.mp3'],
+         base_dict['path16.mp3'], base_dict['path20.mp3'], base_dict['path21.mp3']]}
 
-def play_random(playlista,baza):
-    playlista=[*playlista] #lub copy.deepcopy(playlista)
+
+def play_random(playlista, baza):
+    playlista = [*playlista]  # lub copy.deepcopy(playlista)
     random.shuffle(playlista)
     for song in playlista:
-        song[3]=int(song[3])+1 #song[3] to liczba odsłuchań
-        #kluczem w słowniku z wszystkimi piosenkami jest ścieżka dostępu, którą przechowujemy także w song[0]
-        #baza[song[0]][3]=int(baza[song[0]][3])+1 jeśli użyjesz deepcopy()
-        song[3]=str(song[3]) #Konwertujemy z powrotem na string
-        Play(song[0]) #przekazujemy ściężkę dostępu piosenki
+        song[3] = int(song[3]) + 1  # song[3] to liczba odsłuchań
+        # kluczem w słowniku z wszystkimi piosenkami jest ścieżka dostępu, którą przechowujemy także w song[0]
+        # baza[song[0]][3]=int(baza[song[0]][3])+1 jeśli użyjesz deepcopy()
+        song[3] = str(song[3])  # Konwertujemy z powrotem na string
+        Play(song[0])  # przekazujemy ścieżkę dostępu piosenki
         print(f"Teraz gra piosenka '{song[2]}', autora {song[1]}, którą odsłuchujesz {song[3]} raz.")
-        #Nadpisujemy naszą bazę w .txt
+        # Nadpisujemy naszą bazę w .txt
         values = [';'.join(item) for item in baza.values()]
         open('Dane/baza_piosenek.txt', 'w', -1, 'utf-8').write('\n'.join(values))
 
-print(base_dict) #przed odsłuchaniem
-#list_name = 'Różne'
-list_name=input("Podaj nazwę playlisty: 'Różne' albo 'Instrumental' ")
-want_play = input("Czy chesz puścić liste - 't'  tak, cokolwiek innego - nie ")
-if want_play == 't' :
-    play_random(playlist_base.get(list_name, "Brak takiej playlisty"),base_dict)
-print(base_dict) #po odsłuchaniu
 
+print(base_dict)  # przed odsłuchaniem
+# list_name = 'Różne'
+list_name = input("Podaj nazwę playlisty: 'Różne' albo 'Instrumental' ")
+
+
+brak = "Brak takiej playlisty"
+if playlist_base.get(list_name, brak) == brak:
+    print(brak)
+else:
+    want_play = input("Czy chcesz puścić listę - 't'  tak, cokolwiek innego - nie ")
+    if want_play == 't':
+        play_random(playlist_base.get(list_name, "Brak takiej playlisty"), base_dict)
+    print(base_dict)  # po odsłuchaniu
--- timeit_my_version.py
+++ timeit_my_version.py
@@ -3,6 +3,8 @@ import numpy as np
 import copy
 import random
 
+# https://analityk.edu.pl/kopiowanie-list-i-slownikow/
+
 num_iter_big = 1_000_000
 num_iter_small = 100
 
@@ -31,13 +33,14 @@ def calculate_time_of_copy(num_iter, element, name):
     wyniki = [["List comprehension =>", czas1], ["copy.deepcopy() =>", czas2], ["[0:len(numbers)] =>", czas3],
               ["copy.copy() =>", czas4], ["[:] =>", czas5], ["*args =>", czas6], ["*1 =>", czas7],
               ["Plain loop  =>", czas8], ["Conversion list() =>", czas9], ["extend() =>", czas10]
-              #, ["Build-in copy() =>", czas11], ["np.copy =>", czas12], ["*args =>", czas13]
+              # , ["Build-in copy() =>", czas11], ["np.copy =>", czas12], ["*args =>", czas13]
               ]
 
     [print(wynik[0], wynik[1]) for wynik in sorted(wyniki, key=lambda x: x[1])]
 
     print('**********************************************************************************')
     return wyniki
+
 
 calculate_time_of_copy(num_iter_big, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], "simple list")
 
@@ -48,24 +51,23 @@ calculate_time_of_copy(num_iter_small, np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9]]), 
 calculate_time_of_copy(num_iter_small, np.array([random.sample(range(7_000_000), 1_000_000)]), "random numpy array")
 
 
-
 def calculate_time_of_copy2(num_iter, element, name):
     print("\n Statistics of copy different methods: " + name + " of :" + str(num_iter) + ' iteration\n')
     arr = element
     import copy
     import numpy as np
-    czas1=timeit.timeit(stmt="""arr2=np.array([item for item in arr])""", globals=locals(), number=num_iter)
-    czas2=timeit.timeit(stmt="""arr2=copy.deepcopy(arr)""", globals=locals(), number=num_iter)
-    czas3=timeit.timeit(stmt="""arr2=arr[0:len(arr)]""", globals=locals(),number=num_iter)
-    czas4=timeit.timeit(stmt="""arr2=copy.copy(arr)""", globals=locals(), number=num_iter)
-    czas5=timeit.timeit(stmt="""arr2=arr[:]""", globals=locals(), number=num_iter)
-    czas6=timeit.timeit(stmt="""arr2=np.array([*arr])""", globals=locals(), number=num_iter)
-    czas7=timeit.timeit(stmt="""arr2=arr*1""", globals=locals(), number=num_iter)
-    #czas8=timeit.timeit(stmt="""arr2=[]; for item in arr: arr2.append(item)""", globals=locals(), number=num_iter)
-    czas9=timeit.timeit(stmt="""arr2=np.array(arr)""", globals=locals(), number=num_iter)
-    #czas10=timeit.timeit(stmt="""arr2=[]; arr2.extend(arr)""", globals=locals(), number=num_iter)
-    czas11=timeit.timeit(stmt="""arr2=arr.copy()""", globals=locals(), number=num_iter)
-    czas12=timeit.timeit(stmt="""arr2=np.copy(arr)""", globals=locals(), number=num_iter)
+    czas1 = timeit.timeit(stmt="""arr2=np.array([item for item in arr])""", globals=locals(), number=num_iter)
+    czas2 = timeit.timeit(stmt="""arr2=copy.deepcopy(arr)""", globals=locals(), number=num_iter)
+    czas3 = timeit.timeit(stmt="""arr2=arr[0:len(arr)]""", globals=locals(), number=num_iter)
+    czas4 = timeit.timeit(stmt="""arr2=copy.copy(arr)""", globals=locals(), number=num_iter)
+    czas5 = timeit.timeit(stmt="""arr2=arr[:]""", globals=locals(), number=num_iter)
+    czas6 = timeit.timeit(stmt="""arr2=np.array([*arr])""", globals=locals(), number=num_iter)
+    czas7 = timeit.timeit(stmt="""arr2=arr*1""", globals=locals(), number=num_iter)
+    # czas8=timeit.timeit(stmt="""arr2=[]; for item in arr: arr2.append(item)""", globals=locals(), number=num_iter)
+    czas9 = timeit.timeit(stmt="""arr2=np.array(arr)""", globals=locals(), number=num_iter)
+    # czas10=timeit.timeit(stmt="""arr2=[]; arr2.extend(arr)""", globals=locals(), number=num_iter)
+    czas11 = timeit.timeit(stmt="""arr2=arr.copy()""", globals=locals(), number=num_iter)
+    czas12 = timeit.timeit(stmt="""arr2=np.copy(arr)""", globals=locals(), number=num_iter)
 
     wyniki = [["List comprehension =>", czas1], ["copy.deepcopy() =>", czas2], ["[0:len(numbers)] =>", czas3],
               ["copy.copy() =>", czas3], ["[:] =>", czas5], ["*args =>", czas6], ["*1 =>", czas7],
@@ -77,4 +79,3 @@ def calculate_time_of_copy2(num_iter, element, name):
 calculate_time_of_copy2(num_iter_small, np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9]]), "simple array")
 
 calculate_time_of_copy2(num_iter_small, np.array([random.sample(range(7_000_000), 1_000_000)]), "random array")
-
